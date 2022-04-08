1dcf4199f654f66a43c7e34d28820034669fa38b
Joel Lake <joelcooperlake@gmail.com>
2022-04-08 15:46:30 -0700

    add proper spacing and indenting in HTML file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    let question3 = parseInt($("input:radio[name=vacation]:checked").val());
-    let question5 = parseInt($("input:radio[name=level]:checked").val());
+    let question3 = parseInt($("input:radio[name=question3]:checked").val());
+    let question5 = parseInt($("input:radio[name=question5]:checked").val());
     if (question3 + question5 === 2) {
-      $(".cSharp").show();
-      $(".javaScript").hide();
-      $(".python").hide();
-    } else if (question3 + question5 >= 3) {
-      $(".cSharp").hide();
-      $(".javaScript").show();
-      $(".python").hide();
-    } else if (question3 + question5 >= 4) {
-      $(".cSharp").hide();
-      $(".javaScript").hide();
-      $(".python").show();
+      $("#cSharp").show();
+      $("#javaScript").hide();
+      $("#python").hide();
+    } else if (question3 + question5 === 3) {
+      $("#cSharp").hide();
+      $("#javaScript").show();
+      $("#python").hide();
+    } else if (question3 + question5 === 4) {
+      $("#cSharp").hide();
+      $("#javaScript").hide();
+      $("#python").show();
     } event.preventDefault();
   });
 });
