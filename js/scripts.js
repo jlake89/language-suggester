b879f985c71fd0b4c2fac2714f817f83bf40d129
Joel Lake <joelcooperlake@gmail.com>
2022-04-08 10:04:54 -0700

    WIP working on JS, making other adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
+function testFunction(level) {
+  if (level === "ok") {
+    console.log("input#c")
+  }
+  }
+
+
+
+
+
 $(document).ready(function() {
   $("#form-group").submit(function() {
     $(".name").append();
 
-    $("learn-this").show();
+    $("learnThis").show();
   });
 });
 
