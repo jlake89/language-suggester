266ce1811c29ff0bdb6798280b84aa4d5d827dad
Joel Lake <joelcooperlake@gmail.com>
2022-04-08 13:19:47 -0700

    add and adjusting JS WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-const level = $("input:radio[name=level]:checked").val();
-
-if (level === 'ok') {
-  $('#javaScript').show();
-} else if (level === 'good') {
-  $('#learnThis, #c').show();
-} else {
-  $('#python').show();
-}
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    let vacation = parseInt($("input:radio[name=vacation]:checked").val());
+    let level = parseInt($("input:radio[name=level]:checked").val());
+    if (vacation + level <= 2) {
+      $(".cSharp").show();
+      $(".javaScript").hide();
+      $(".python").hide();
+    } else if (vacation + level <= 11) {
+      $(".cSharp").hide();
+      $(".javaScript").show();
+      $(".python").hide();
+    } else if (vacation + level <= 15) {
+      $(".cSharp").hide();
+      $(".javaScript").hide();
+      $(".python").show();
+    } event.preventDefault();
+  });
+});
